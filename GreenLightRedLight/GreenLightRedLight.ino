2559f7a514d60e15be1e1ef1a10fd131d413633d
jsnam <jsnam3379@gmail.com>
2021-11-06 14:07:21 +0900

    feat: update (improved playTheme function and added button features)

--- GreenLightRedLight/GreenLightRedLight.ino
+++ GreenLightRedLight/GreenLightRedLight.ino
@@ -1,20 +1,16 @@
 /*
    Green Light Red Light Game
 
-   Things to consider:
-   1. Noise detection (hc-sro4)
-   2. Hardware malfunction (wires)
-   3.
+   Game Progress
+   1. Game Start button
+   2. Main music plays
+   3. Game music plays
+   4. Distance detection
+   5. Gane Won/Lost
 
-   Things to implement:
-   1. Game start and quit (win) button
-   2. Game win/lost music
-   3. RGB LED
-
-   Naming Conventions:
-   snake_case: general variables
-   camelCase: pin variables, functions
-   PascalCase: file names
+   Things to Do
+   - adjust button sensitivity (debounce time)
+   - adjust distance difference
 */
 
 
@@ -25,7 +21,8 @@ int btnState;
 
 // servo pin and variables
 #include <Servo.h>
-int pos = 0;
+int posToWall = 0;
+int posToPlayer = 180;
 int servoPin = 9;
 Servo servo;
 
@@ -44,19 +41,70 @@ const int greenPin = 7;
 const int redPin = 8;
 
 // passive buzzer pins
+#include "pitches.h"
 const int musicPin = 2;
 const int alarmPin = 3;
 
-// green light red light theme
-char game_notes[] = "eaaagaaeeg";
-int game_beats[] = {1, 1, 2, 2, 2, 1, 1, 1, 1, 1};
+// main theme (squid game)
+int main_melodies[] = {
+  NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4,
+  NOTE_DS5, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_A4, NOTE_B4,
+  NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4,
+  NOTE_B4, NOTE_A4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_E4, NOTE_E4
+};
+int main_durations[] = {
+  4, 4, 2, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 2,
+  4, 4, 2, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 2
+};
+int main_theme_len = sizeof(main_melodies) / sizeof(int);
 
-// squid game theme
-char main_notes[] = "bbbbbbbbbagabbbbbbbbagagee";
-int main_beats[] = {1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2};
+// game theme (green light red light)
+int game_melodies[] = {
+  NOTE_E4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_G4,
+  NOTE_A4, NOTE_A4, NOTE_E4, NOTE_E4, NOTE_G4
+};
+int game_durations[] = {
+  4, 4, 2, 2, 2,
+  4, 4, 4, 4, 2
+};
+int game_theme_len = sizeof(game_melodies) / sizeof(int);
+
+// win theme (mario victory)
+int win_melodies[] = {
+  NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, NOTE_E5,
+  NOTE_G5, NOTE_E5,
+  NOTE_GS3, NOTE_C4, NOTE_DS4, NOTE_GS4, NOTE_C5, NOTE_DS5,
+  NOTE_GS5, NOTE_DS5,
+  NOTE_AS3, NOTE_D4, NOTE_F4, NOTE_AS4, NOTE_D5, NOTE_F5,
+  NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_C6
+};
+int win_durations[] = {
+  6, 6, 6, 6, 6, 6,
+  2, 2,
+  6, 6, 6, 6, 6, 6,
+  2, 2,
+  6, 6, 6, 6, 6, 6,
+  2, 6, 6, 6, 1
+};
+int win_theme_len = sizeof(win_melodies) / sizeof(int);
+
+// lost theme
+int lost_melodies[] = {
+  NOTE_C5, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4,
+  NOTE_A4, NOTE_G4, NOTE_F4, NOTE_C4
+};
+int lost_durations[] = {
+  6, 6, 6, 6, 6, 6,
+  6, 6, 6, 2
+};
+int lost_theme_len = sizeof(lost_melodies) / sizeof(int);
 
 
 void setup() {
+  Serial.begin(9600);
+
   // hc-sr04 sensor pins
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -68,108 +116,114 @@ void setup() {
   digitalWrite(redPin, LOW);
 
   // button setup
-  button.setDebounceTime(100);
+  button.setDebounceTime(1000);
+
+  // servo setup
+  servo.attach(servoPin, 500, 2500);
+  servo.write(posToWall);
 
   // passive buzzer setup
   pinMode(musicPin, OUTPUT);
   pinMode(alarmPin, OUTPUT);
   digitalWrite(musicPin, LOW);
   digitalWrite(alarmPin, LOW);
+  Serial.println("***********************");
+  Serial.println("Playing main music ...");
+  playTheme(main_melodies, main_durations, main_theme_len);
 
-  // servo setup
-  servo.attach(servoPin, 500, 2500);
-  servo.write(0);
-  Serial.begin(9600);
-
-  playTheme(main_notes, main_beats);
   delay(2000);
 }
 
+
 void loop()
 {
-  // green light
-  servo.write(0);
-  digitalWrite(greenPin, HIGH);
-  digitalWrite(redPin, LOW);
-  playTheme(game_notes, game_beats);
+  // game counts
+  for (int game_cnt = 0; game_cnt < 10; game_cnt++)
+  {
+    // print game count
+    Serial.println("***********************");
+    Serial.print("\t");
+    Serial.print("Game ");
+    Serial.println(game_cnt + 1);
 
-  // red light
-  servo.write(180);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(redPin, HIGH);
-  delay(1000);
+    // button not pressed (game continues)
+    if (!isButtonPressed())
+    {
+      // green light
+      servo.write(posToWall);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(redPin, LOW);
+      Serial.println("-----------------------");
+      Serial.println("Green Light ...");
+      playTheme(game_melodies, game_durations, game_theme_len);
 
-  // read stop distance
-  // TODO: fix (first distance = 0 cm error)
-  Serial.println("***********************");
-  Serial.print("Stop ");
-  stop_distance = readDistance(300);
+      // red light
+      Serial.println("Red   Light !");
+      servo.write(posToPlayer);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(redPin, HIGH);
+      delay(1000);
+
+      // read stop distance
+      // TODO: fix (first distance = 0 cm error)
+      Serial.println("-----------------------");
+      Serial.print("Stop ");
+      stop_distance = readDistance(300);
+      Serial.println("-----------------------");
+
+      // read move distances (takes (read_cnt x read_time) seconds)
+      for (int read_cnt = 0; read_cnt < 25; read_cnt ++)
+      {
+        Serial.print("Move ");
+        move_distance = readDistance(80);
+
+        // player move (game over)
+        if (isPlayerMove())
+        {
+          Serial.println("***********************");
+          playAlarm();
+          delay(1000);
+          playTheme(lost_melodies, lost_durations, lost_theme_len);
+          exit(0);
+        }
+
+        // player stay still (game continues)
+        else
+        {
+          continue;
+        }
+      }
+    }
+
+    // button pressed (game won)
+    else if (isButtonPressed())
+    {
+      Serial.println("-----------------------");
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(redPin, LOW);
+      playTheme(win_melodies, win_durations, win_theme_len);
+      exit(0);
+    }
+  }
+
+  // game count over (game over)
   Serial.println("-----------------------");
-
-  // read move distances (takes (read_cnt x read_time) seconds)
-  for (int read_cnt = 0; read_cnt < 20; read_cnt ++)
-  {
-    Serial.print("Move ");
-    move_distance = readDistance(100);
-
-    // player move
-    if (isMove())
-    {
-      playAlarm();
-      break;
-    }
-
-    // player stay still
-    else
-    {
-      continue;
-    }
-  }
+  Serial.println("Out of count. You lost!");
+  Serial.println("***********************");
+  playTheme(lost_melodies, lost_durations, lost_theme_len);
+  exit(0);
 }
 
-int frequency(char note)
+void playTheme(int melodies[], int durations[], int theme_length)
 {
-  char names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C', 'D', 'E'};
-  int num_notes = strlen(names);
-  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523, 587, 659};
-
-  for (int i = 0; i < num_notes; i++)
+  for (int i = 0; i < theme_length; i++)
   {
-    if (names[i] == note)
-    {
-      return (frequencies[i]);
-    }
-  }
+    int duration = 1000 / durations[i];
+    int pause_time = duration * 1.13;
 
-  return 0;
-}
-
-void playTheme(char notes[], int beats[])
-{
-  int tempo = 280;
-  int song_length = strlen(notes);
-  int music_duration;
-
-  // loop through song
-  for (int i = 0; i < song_length; i++)
-  {
-    music_duration = beats[i] * tempo;
-
-    // pause sound
-    if (notes[i] == ' ')
-    {
-      delay(music_duration);
-    }
-
-    // play sound
-    else
-    {
-      tone(musicPin, frequency(notes[i]), music_duration);
-      delay(music_duration);
-    }
-
-    // brief pause between notes
-    delay(tempo / 10);
+    tone(musicPin, melodies[i], duration);
+    delay(pause_time);
+    noTone(musicPin);
   }
 }
 
@@ -188,27 +242,350 @@ int readDistance(int read_time)
   // show distance and button state
   Serial.print("Distance = ");
   Serial.print(distance);
-  Serial.println(" cm  ");
+  Serial.println(" cm");
   delay(read_time);
 
   return distance;
 }
 
-bool isMove()
+bool isPlayerMove()
 {
   diff = abs(stop_distance - move_distance);
 
-  // player move (check only in this range for noise prevention)
-  if (2 <= diff && diff < 15)
+  // player move (game lost)
+  if (7 < diff && diff < 10) // noise removal
   {
+    Serial.println("-----------------------");
+    Serial.println("Player 324 eliminated !");
     return true;
   }
 
-  // player stay still
+  // player stay still (game continue)
   else
   {
     return false;
   }
+}
+
+bool isButtonPressed()
+{
+  button.loop();
+  if (button.isPressed())
+  {
+    Serial.println("***********************");
+    Serial.println("Button is pressed!");
+    Serial.println("***********************");
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+    playTheme(win_melodies, win_durations, win_theme_len);
+    exit(0);
+
+
+    return true;
+  }
+  Serial.println("***********************");
+  Serial.println("Button is not pressed!");
+  return false;
+}
+
+void playAlarm()
+{
+  tone(alarmPin, 600, 3000);
+  delay(500);
+  noTone(alarmPin);
+  delay(500);
+  tone(alarmPin, 600, 3000);
+  delay(500);
+  noTone(alarmPin);
+  delay(500);
+}/*
+   Green Light Red Light Game
+
+   Game Progress
+   1. Game Start button
+   2. Main music plays
+   3. Game music plays
+   4. Distance detection
+   5. Gane Won/Lost
+
+   Things to Do
+   - adjust button sensitivity (debounce time)
+   - adjust distance difference
+*/
+
+
+// button pins and variables
+#include <ezButton.h>
+ezButton button(12);
+int btnState;
+
+// servo pin and variables
+#include <Servo.h>
+int posToWall = 0;
+int posToPlayer = 180;
+int servoPin = 9;
+Servo servo;
+
+// hc-sr04 sensor pins and variables
+// optimal range: 2 cm ~ 400 cm
+const int trigPin = 5;
+const int echoPin = 6;
+long read_duration;
+int distance;
+int stop_distance;
+int move_distance;
+int diff;
+
+// led pins
+const int greenPin = 7;
+const int redPin = 8;
+
+// passive buzzer pins
+#include "pitches.h"
+const int musicPin = 2;
+const int alarmPin = 3;
+
+// main theme (squid game)
+int main_melodies[] = {
+  NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4,
+  NOTE_DS5, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_A4, NOTE_B4,
+  NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4,
+  NOTE_B4, NOTE_A4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_E4, NOTE_E4
+};
+int main_durations[] = {
+  4, 4, 2, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 2,
+  4, 4, 2, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 2
+};
+int main_theme_len = sizeof(main_melodies) / sizeof(int);
+
+// game theme (green light red light)
+int game_melodies[] = {
+  NOTE_E4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_G4,
+  NOTE_A4, NOTE_A4, NOTE_E4, NOTE_E4, NOTE_G4
+};
+int game_durations[] = {
+  4, 4, 2, 2, 2,
+  4, 4, 4, 4, 2
+};
+int game_theme_len = sizeof(game_melodies) / sizeof(int);
+
+// win theme (mario victory)
+int win_melodies[] = {
+  NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, NOTE_E5,
+  NOTE_G5, NOTE_E5,
+  NOTE_GS3, NOTE_C4, NOTE_DS4, NOTE_GS4, NOTE_C5, NOTE_DS5,
+  NOTE_GS5, NOTE_DS5,
+  NOTE_AS3, NOTE_D4, NOTE_F4, NOTE_AS4, NOTE_D5, NOTE_F5,
+  NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_AS5, NOTE_C6
+};
+int win_durations[] = {
+  6, 6, 6, 6, 6, 6,
+  2, 2,
+  6, 6, 6, 6, 6, 6,
+  2, 2,
+  6, 6, 6, 6, 6, 6,
+  2, 6, 6, 6, 1
+};
+int win_theme_len = sizeof(win_melodies) / sizeof(int);
+
+// lost theme
+int lost_melodies[] = {
+  NOTE_C5, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4,
+  NOTE_A4, NOTE_G4, NOTE_F4, NOTE_C4
+};
+int lost_durations[] = {
+  6, 6, 6, 6, 6, 6,
+  6, 6, 6, 2
+};
+int lost_theme_len = sizeof(lost_melodies) / sizeof(int);
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // hc-sr04 sensor pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // led setup
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(redPin, LOW);
+
+  // button setup
+  button.setDebounceTime(1000);
+
+  // servo setup
+  servo.attach(servoPin, 500, 2500);
+  servo.write(posToWall);
+
+  // passive buzzer setup
+  pinMode(musicPin, OUTPUT);
+  pinMode(alarmPin, OUTPUT);
+  digitalWrite(musicPin, LOW);
+  digitalWrite(alarmPin, LOW);
+  Serial.println("***********************");
+  Serial.println("Playing main music ...");
+  playTheme(main_melodies, main_durations, main_theme_len);
+
+  delay(2000);
+}
+
+
+void loop()
+{
+  // game counts
+  for (int game_cnt = 0; game_cnt < 10; game_cnt++)
+  {
+    // print game count
+    Serial.println("***********************");
+    Serial.print("\t");
+    Serial.print("Game ");
+    Serial.println(game_cnt + 1);
+
+    // button not pressed (game continues)
+    if (!isButtonPressed())
+    {
+      // green light
+      servo.write(posToWall);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(redPin, LOW);
+      Serial.println("-----------------------");
+      Serial.println("Green Light ...");
+      playTheme(game_melodies, game_durations, game_theme_len);
+
+      // red light
+      Serial.println("Red   Light !");
+      servo.write(posToPlayer);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(redPin, HIGH);
+      delay(1000);
+
+      // read stop distance
+      // TODO: fix (first distance = 0 cm error)
+      Serial.println("-----------------------");
+      Serial.print("Stop ");
+      stop_distance = readDistance(300);
+      Serial.println("-----------------------");
+
+      // read move distances (takes (read_cnt x read_time) seconds)
+      for (int read_cnt = 0; read_cnt < 25; read_cnt ++)
+      {
+        Serial.print("Move ");
+        move_distance = readDistance(80);
+
+        // player move (game over)
+        if (isPlayerMove())
+        {
+          Serial.println("***********************");
+          playAlarm();
+          delay(1000);
+          playTheme(lost_melodies, lost_durations, lost_theme_len);
+          exit(0);
+        }
+
+        // player stay still (game continues)
+        else
+        {
+          continue;
+        }
+      }
+    }
+
+    // button pressed (game won)
+    else if (isButtonPressed())
+    {
+      Serial.println("-----------------------");
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(redPin, LOW);
+      playTheme(win_melodies, win_durations, win_theme_len);
+      exit(0);
+    }
+  }
+
+  // game count over (game over)
+  Serial.println("-----------------------");
+  Serial.println("Out of count. You lost!");
+  Serial.println("***********************");
+  playTheme(lost_melodies, lost_durations, lost_theme_len);
+  exit(0);
+}
+
+void playTheme(int melodies[], int durations[], int theme_length)
+{
+  for (int i = 0; i < theme_length; i++)
+  {
+    int duration = 1000 / durations[i];
+    int pause_time = duration * 1.13;
+
+    tone(musicPin, melodies[i], duration);
+    delay(pause_time);
+    noTone(musicPin);
+  }
+}
+
+int readDistance(int read_time)
+{
+  // send ping from trigger pin
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(10);
+
+  // read ping from echo pin
+  read_duration = pulseIn(echoPin, HIGH);
+  distance = (read_duration * 0.0343) / 2.;
+
+  // show distance and button state
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(read_time);
+
+  return distance;
+}
+
+bool isPlayerMove()
+{
+  diff = abs(stop_distance - move_distance);
+
+  // player move (game lost)
+  if (7 < diff && diff < 10) // noise removal
+  {
+    Serial.println("-----------------------");
+    Serial.println("Player 324 eliminated !");
+    return true;
+  }
+
+  // player stay still (game continue)
+  else
+  {
+    return false;
+  }
+}
+
+bool isButtonPressed()
+{
+  button.loop();
+  if (button.isPressed())
+  {
+    Serial.println("***********************");
+    Serial.println("Button is pressed!");
+    Serial.println("***********************");
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+    playTheme(win_melodies, win_durations, win_theme_len);
+    exit(0);
+
+
+    return true;
+  }
+  Serial.println("***********************");
+  Serial.println("Button is not pressed!");
+  return false;
 }
 
 void playAlarm()
